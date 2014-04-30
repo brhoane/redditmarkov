fb38545ac03ac8a03a157972a120f5115c23caf1
Andrew Zeng <zeng.qed@gmail.com>
2014-04-29 22:38:27 -0400

    Modified to output JSON for clarity

---
+++ download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download.sh
+
+
+if [ -z "$1" ]; then
+    echo "$0 subreddit_list_file"
+    exit 1
+fi
+
+for i in `cat ${1}`; do
+    python rlist.py $i
+	python markov.py $i
+done
