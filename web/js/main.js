b568fdb923f229700e563901bd546a4c07dabf13
Drew Inglis <drew@drewinglis.com>
2014-05-02 00:11:05 -0400

    refactor so you can do "http://localhost:4000/?subreddit=all" to specify subreddit

--- web/js/main.js
+++ web/js/main.js
@@ -1,21 +1,25 @@
 $(function () {
+  var $posts = $(".content .posts");
+  var params = getSearchParameters();
+  var subreddit = params.subreddit || "programming";
 
-  $posts = $(".content .posts");
-  
-  $.getJSON("/data/programming.json", function (data) {
-  
-	for (var i = 2; i <= data.titles.length; i += 1) {
-		var child = $posts.children().eq(0).clone();
-		child.find(".rank").text(i);
-		$posts.append(child);
-	}
-	renderSubreddit(data);
-  })
+  $.getJSON("/data/" + subreddit + ".json", function (data) {
+    for (var i = 2; i <= data.titles.length; i += 1) {
+      var child = $posts.children().eq(0).clone();
+      child.find(".rank").text(i);
+      $posts.append(child);
+    }
+
+    renderSubreddit(data);
+  });
 
 
   function renderSubreddit (subreddit) {
+    var maxPosts = subreddit.titles.length > 25 ? 25 : subreddit.titles.length;
+
     $(".subreddit-name").text(subreddit.title);
-    for (var ii = 0; ii <= subreddit.titles.length; ii += 1) {
+
+    for (var ii = 0; ii <= maxPosts; ii += 1) {
       renderPost($posts.children().eq(ii), subreddit.titles[ii]);
     }
   }
@@ -29,5 +33,23 @@ $(function () {
 
   function randInt (max) {
     return Math.floor(Math.random() * max);
+  }
+
+  /* Code to get query parameters from the url copied from:
+   * http://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
+   */
+  function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+  }
+
+  function transformToAssocArray(prmstr) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for (var i = 0; i < prmarr.length; i++) {
+      var tmparr = prmarr[i].split("=");
+      params[tmparr[0]] = tmparr[1];
+    }
+    return params;
   }
 });
